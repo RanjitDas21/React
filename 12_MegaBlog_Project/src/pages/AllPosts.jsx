901be71a097027e30cff5862863a704e2a2b0e4d
Ranjit Das <ranjitdasajodhyapur38@gmail.com>
2025-09-05 19:22:13 +0530

    Add routing and page components for authentication and post management

---
+++ 12_MegaBlog_Project/src/pages/AllPosts.jsx
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import Container from '../components/container/Container.jsx';
+import PostCard from '../components/PostCard.jsx';
+import service from '../appwrite/configuration.js';
+ 
+function AllPosts() {
+    const [posts, setPosts] = useState([])
+    useEffect(() => {}, [])
+    service.getPosts([]).then((posts) => {
+        if (posts) {
+            setPosts(posts.documents)
+        }
+    })
+  return (
+    <div className='w-full py-8'>
+        <Container>
+            <div className='flex flex-wrap'>
+                {posts.map((post) => (
+                    <div key={post.$id} className='p-2 w-1/4'>
+                        <PostCard {...post} />
+                    </div>
+                ))}
+            </div>
+            </Container>
+    </div>
+  )
+}
+
+export default AllPosts
